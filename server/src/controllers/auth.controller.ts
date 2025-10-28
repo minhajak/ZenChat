26f379387cb96e75d59ce6371be6156d0fd27809
minhajak <minhajpty@gmail.com>
2025-10-28 13:09:49 +0530

    fix 5

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -17,18 +17,22 @@ import mongoose from "mongoose";
 import cloudinary from "../config/cloudinary.config";
 import { Friend } from "../models/friend.model";
 
-declare global {
-	namespace Express {
-		interface User {
-			role: string;
-			userId: mongoose.Types.ObjectId;
-		}
+import "express";
 
-		interface Request {
-			user?: User;
-		}
-	}
+
+declare global {
+  namespace Express {
+    interface User {
+      role: string;
+      userId: mongoose.Types.ObjectId;
+    }
+
+    interface Request {
+      user?: User;
+    }
+  }
 }
+
 
 
 export const signUp = async (req: Request, res: Response): Promise<void> => {
